809792f18ff91f3e342023cb807e881f30d19f35
zakrwn <45009413+zakrwn@users.noreply.github.com>
2018-12-14 20:54:37 +0400

    Update bot.js

--- bot.js
+++ bot.js
@@ -2,7 +2,7 @@ const Discord = require('discord.js'),
     bot = new Discord.Client({sisableEveryone: true})
     console.log("muuuuuuuuute,");
 bot.on('guildMemberAdd', member => {
-const mohamed= member.guild.channels.get("ايدي الشات");
+const mohamed= member.guild.channels.get("518932875958288384");
 if(!mohamed) return;
 if(mohamed) {
 setTimeout(() => mohamed.send(**We .**), 4000)
