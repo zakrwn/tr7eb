0c0cead9062414a84889c58d10702805cd7a2409
zakrwn <45009413+zakrwn@users.noreply.github.com>
2018-12-15 09:47:01 +0400

    Update bot.js

--- bot.js
+++ bot.js
@@ -5,7 +5,7 @@ client.on('guildMemberAdd', member => {
 const mohamed= member.guild.channels.get("518932875958288384");
 if(!mohamed) return;
 if(mohamed) {
-setTimeout(() => mohamed.send(`Welcome`), 4000)        
+setTimeout(() => mohamed.send(` ** Welcome To Brilliance 🎧🧡 . . **`), 4000)        
 }
 });
 
